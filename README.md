69514c41d0cf32cf2bcaecf0d0a588052ff824bd
hosomi <kiyotake.hosomi@outlook.jp>
2020-06-17 12:55:57 +0900

    Sample の構成準備。

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # spring-cloud-function-example
+
+:link: [Azure での Spring Cloud Function の概要 | Microsoft Docs](https://docs.microsoft.com/ja-jp/azure/developer/java/spring-framework/getting-started-with-spring-cloud-function-in-azure#create-a-new-maven-project)
---
+++ hello/src/main/java/com/example/HelloFunction.java
@@ -0,0 +1,23 @@
+package com.example;
+
+import java.util.function.Function;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.example.model.Greeting;
+import com.example.model.User;
+
+@SpringBootApplication
+public class HelloFunction {
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(HelloFunction.class, args);
+    }
+
+    @Bean
+    public Function<User, Greeting> hello() {
+        return user -> new Greeting("Welcome, " + user.getName());
+    }
+}
